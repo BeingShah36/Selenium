2105c11a2a48dd794898c67c259240b421d89a4e
BeingShah36 <shahbazalam.sayyad@gmail.com>
2019-04-15 03:50:07 -0700

    Verifying and storing the result of edit box of facebook is enabled or not

---
+++ Shahenshah.java
@@ -0,0 +1,25 @@
+package newpackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Shahenshah {
+
+	public static void main(String[] args) throws InterruptedException {
+	 System.setProperty("webdriver.gecko.driver","D:\\programs java\\geckodriver-v0.24.0-win64\\geckodriver.exe");
+	WebDriver driver=new FirefoxDriver();
+	driver.get("https://www.facebook.com/");
+	//WebElement shah=driver.findElement(By.name("email"));
+	//shah.sendKeys("8951101707");
+	//Thread.sleep(4000);
+WebElement shah=driver.findElement(By.xpath("//*[@id=\"u_0_9\"]"));
+shah.click();
+System.out.println(shah.isSelected());
+Thread.sleep(4000);
+	boolean abd=shah.isSelected();
+System.out.println(abd);
+	}
+
+}
