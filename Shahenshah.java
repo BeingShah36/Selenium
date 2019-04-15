549d89964ff2361b143aeed525e9a9c7d9583a34
BeingShah36 <shahbazalam.sayyad@gmail.com>
2019-04-15 03:54:46 -0700

    Verifying and storing the result of edit box of facebook is enabled or not

--- Shahenshah.java
+++ Shahenshah.java
@@ -2,7 +2,7 @@ package newpackage;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebElement; 
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Shahenshah {
